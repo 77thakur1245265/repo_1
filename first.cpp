f74274020c50a9b523ffc45bebcb0b032af6dd65
Sudhanshu Thakur <147124410+77thakur1245265@users.noreply.github.com>
2024-06-25 16:44:01 +0530

    25-june-2

--- first.cpp
+++ first.cpp
@@ -1,6 +1,40 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int *p;
-    cout<<p<<endl;
+    int n=5;
+    (n%2==0)? cout<<"even"<<endl : cout<<"odd"<<endl;
+
+    char ch;
+    cout << "Enter: "<<endl;
+    cin>>ch;
+    if(ch>=65 && ch<=90 || ch>=97 && ch<=122){
+        switch (ch)
+    {
+    case 'a' : cout<<"vowel"<<endl;
+        break;
+    case 'e' : cout<<"vowel"<<endl;
+        break;
+    case 'i' : cout<<"vowel"<<endl;
+        break;
+    case 'o' : cout<<"vowel"<<endl;
+        break;
+    case 'u' : cout<<"vowel"<<endl;
+        break;
+    case 'A' : cout<<"vowel"<<endl;
+        break;
+    case 'E' : cout<<"vowel"<<endl;
+        break;
+    case 'I' : cout<<"vowel"<<endl;
+        break;
+    case 'O' : cout<<"vowel"<<endl;
+        break;
+    case 'U' : cout<<"vowel"<<endl;
+        break;
+    default:
+    cout<<"consonent";
+        break;
+    }
+    }else{
+        cout<<"ERROR"<<endl;cout<<"Enter an alphabet"<<endl;
+    }
 }
